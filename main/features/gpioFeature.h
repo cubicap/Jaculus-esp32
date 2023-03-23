99068eaf85ad714a1f8e3a40c0e7ec6dea9f7a89
Petr Kubica <cubicap@email.cz>
2023-03-23 18:25:56 +0100

    fix  esp32s3 gpio whitelist

--- main/features/gpioFeature.h
+++ main/features/gpioFeature.h
@@ -45,8 +45,8 @@ class GpioFeature : public Next {
     static inline const std::set<int> DIGITAL_PINS = { 0, 2, 4, 5, 12, 13, 14, 15, 18, 19, 21, 22, 23, 25, 26, 27, 32, 33, 34, 35, 36, 37, 38, 39 };
     static inline const std::set<int> ANALOG_PINS = { 32, 33, 34, 35, 36, 37, 38, 39 };
     static inline const std::set<int> INTERRUPT_PINS = DIGITAL_PINS;
-#elif defined(CONFIG_IDF_TARGET_ESP32S2)
-    static inline const std::set<int> DIGITAL_PINS = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 21, 38, 45, 46 47, 48 };
+#elif defined(CONFIG_IDF_TARGET_ESP32S3)
+    static inline const std::set<int> DIGITAL_PINS = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 21, 38, 45, 46, 47, 48 };
     static inline const std::set<int> ANALOG_PINS = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
     static inline const std::set<int> INTERRUPT_PINS = DIGITAL_PINS;
 #endif
