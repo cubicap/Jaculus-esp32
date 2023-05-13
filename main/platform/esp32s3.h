8c91a342a6a4195296e8294b8a9a7fb474d4d6de
Petr Kubica <cubicap@email.cz>
2023-05-13 19:10:41 +0200

    fix gpio whitelist (overlap with uart0)

--- main/platform/esp32s3.h
+++ main/platform/esp32s3.h
@@ -18,7 +18,7 @@ public:
         struct PinConfig {
             static inline const std::set<int> DIGITAL_PINS = {
                 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 21,
-                33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48
+                33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 45, 46, 47, 48
             };
             static inline const std::unordered_map<int, std::pair<int, int>> ANALOG_PINS = {
                 { 1, { 1, ADC1_GPIO1_CHANNEL } },
