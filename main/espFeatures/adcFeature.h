b51e4b3209d5c19fdb31db94781520e1d333e869
Petr Kubica <cubicap@email.cz>
2024-07-04 14:31:24 +0200

    add configurable attenuation to adc

--- main/espFeatures/adcFeature.h
+++ main/espFeatures/adcFeature.h
@@ -24,7 +24,7 @@ class AdcFeature : public Next {
 
     class Adc {
     public:
-        void configure(int pin) {
+        void configure(int pin, int atten = static_cast<int>(ADC_ATTEN_DB_12)) {
             int adcNum;
             int channel;
             std::tie(adcNum, channel) = getAnalogPin(pin);
@@ -34,7 +34,7 @@ class AdcFeature : public Next {
                 throw std::runtime_error(esp_err_to_name(err));
             }
 
-            err = adc1_config_channel_atten(static_cast<adc1_channel_t>(channel), ADC_ATTEN_DB_12);
+            err = adc1_config_channel_atten(static_cast<adc1_channel_t>(channel), static_cast<adc_atten_t>(atten));
             if (err != ESP_OK) {
                 throw std::runtime_error(esp_err_to_name(err));
             }
@@ -56,10 +56,28 @@ public:
         Next::initialize();
 
         jac::FunctionFactory ff(this->context());
-
         jac::Module& adcModule = this->newModule("adc");
 
-        adcModule.addExport("configure", ff.newFunction(noal::function(&Adc::configure, &adc)));
+        jac::Object attten = jac::Object::create(this->context());
+        attten.defineProperty("Db0", jac::Value::from(this->context(), static_cast<int>(ADC_ATTEN_DB_0)));
+        attten.defineProperty("Db2_5", jac::Value::from(this->context(), static_cast<int>(ADC_ATTEN_DB_2_5)));
+        attten.defineProperty("Db6", jac::Value::from(this->context(), static_cast<int>(ADC_ATTEN_DB_6)));
+        attten.defineProperty("Db12", jac::Value::from(this->context(), static_cast<int>(ADC_ATTEN_DB_12)));
+
+        adcModule.addExport("configure", ff.newFunctionVariadic([this](std::vector<jac::ValueWeak> args) {
+            if (args.size() < 1 || args.size() > 2) {
+                throw std::runtime_error("Invalid number of arguments");
+            }
+
+            int pin = args[0].to<int>();
+            int atten = static_cast<int>(ADC_ATTEN_DB_12);
+            if (args.size() == 2) {
+                atten = args[1].to<int>();
+            }
+
+            adc.configure(pin, atten);
+        }));
         adcModule.addExport("read", ff.newFunction(noal::function(&Adc::read, &adc)));
+        adcModule.addExport("Attenuation", attten);
     }
 };
--- ts-examples/src/adc.ts
+++ ts-examples/src/adc.ts
@@ -11,9 +11,11 @@ const LED_PIN = 45;
 ledc.configureTimer(0, 1000);
 ledc.configureChannel(0, LED_PIN, 0, 1023);
 
+adc.configure(INPUT_PIN);
+
 let power = 3;
 
 setInterval(() => {
     const value = adc.read(INPUT_PIN);
-    ledc.setDuty(0, Math.pow(1023 - value, power) / Math.pow(1023, power - 1));
+    ledc.setDuty(0, Math.pow(value, power) / Math.pow(1023, power - 1));
 }, 10);
